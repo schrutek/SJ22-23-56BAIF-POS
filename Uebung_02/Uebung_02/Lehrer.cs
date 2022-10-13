3c0496e1162cb5da453e6d526aca1ba2ad0893a5
schrutek <martin@schrutek>
2022-10-13 17:11:34 +0200

    Properties Excercise done

---
+++ Uebung_02/Uebung_02/Lehrer.cs
@@ -0,0 +1,39 @@
+﻿namespace ExProperties.App
+{
+    internal class Lehrer
+    {
+        public string Vorname { get; set; }
+
+        public string Zuname { get; set; } = string.Empty;
+
+        public decimal? Bruttogehalt
+        {
+            get { return _bruttogehalt; }
+            set
+            {
+                if (_bruttogehalt is not null)
+                {
+                    _bruttogehalt = value;
+                }
+                else
+                { }
+            }
+        }
+        private decimal? _bruttogehalt;
+
+        public string Kuerzel
+        {
+            get 
+            {
+                if (Zuname?.Length < 3)
+                {
+                    return Zuname?.ToUpper() ?? string.Empty;
+                }
+                //return Zuname?.Substring(0, 3)?.ToUpper() ?? string.Empty;
+                return Zuname?[..3]?.ToUpper() ?? string.Empty;
+            }
+        }
+
+        public decimal Nettogehalt => (_bruttogehalt ?? 0) * 0.8M;
+    }
+}
---
+++ Uebung_02/Uebung_02/Rechteck.cs
@@ -0,0 +1,42 @@
+﻿namespace ExProperties.App
+{
+    internal class Rechteck
+    {
+        private int _laenge;
+        private int _breite;
+
+        public int Laenge
+        {
+            get { return _laenge; }
+            set 
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Ungültige Länge");
+                }
+                else
+                {
+                    _laenge = value;
+                }
+            }
+        }
+
+        public int Breite
+        {
+            get { return _breite; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Ungültige Breite");
+                }
+                else
+                {
+                    _breite = value;
+                }
+            }
+        }
+
+        public int Flaeche => Laenge * Breite;
+    }
+}
